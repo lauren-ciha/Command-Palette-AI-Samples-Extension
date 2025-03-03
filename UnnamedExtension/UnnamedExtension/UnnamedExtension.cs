097c7310361218dd314d7da71760b1bdf6fd74b5
Lauren Ciha <laurenciha@microsoft.com>
2025-03-03 10:55:03 -0500

    Initial commit

---
+++ UnnamedExtension/UnnamedExtension/UnnamedExtension.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.CommandPalette.Extensions;
+
+namespace UnnamedExtension;
+
+[ComVisible(true)]
+[Guid("19404d9b-4eec-4276-ae52-deb60b775709")]
+[ComDefaultInterface(typeof(IExtension))]
+public sealed partial class UnnamedExtension : IExtension, IDisposable
+{
+    private readonly ManualResetEvent _extensionDisposedEvent;
+
+    private readonly UnnamedExtensionCommandsProvider _provider = new();
+
+    public UnnamedExtension(ManualResetEvent extensionDisposedEvent)
+    {
+        this._extensionDisposedEvent = extensionDisposedEvent;
+    }
+
+    public object? GetProvider(ProviderType providerType)
+    {
+        return providerType switch
+        {
+            ProviderType.Commands => _provider,
+            _ => null,
+        };
+    }
+
+    public void Dispose() => this._extensionDisposedEvent.Set();
+}
